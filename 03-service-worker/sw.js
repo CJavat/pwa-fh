266c68ebb8070c5a25554b875653fdeef20b01f3
CJavatX <cdpm98@hotmail.com>
2025-04-02 11:47:36 -0600

    SECCIÓN 5 - TERMINADO

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -1,3 +1,47 @@
-
 // Ciclo de vida del SW
 
+self.addEventListener("install", (event) => {
+  // Descargar assets
+  // Crear caché
+  console.log("SW: Instalando SW");
+
+  // self.skipWaiting();
+
+  const instalacion = new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log("SW: Instalaciones terminadas");
+      self.skipWaiting();
+      res();
+    }, 1);
+  });
+
+  event.waitUntil(instalacion);
+});
+
+// Cuando el SW toma el control de la app
+self.addEventListener("activate", (event) => {
+  // Borrar caché viejo.
+  console.log("SW: Activo y listo para controlar la app");
+});
+
+// Fetch: Manejo de peticiones HTTP
+self.addEventListener("fetch", (event) => {
+  // Aplicar estrategias del caché
+  // console.log("SW", event.request.url);
+  // if (event.request.url.includes("https://reqres.in")) {
+  //   const resp = new Response(`{ ok: false,  messaje: 'jajaja' }`);
+  //   event.respondWith(resp);
+  // }
+});
+
+// SYNC: Recuperamos la conexión a internet.
+// self.addEventListener("sync", (event) => {
+//   console.log("¡Tenemos la conexión!");
+//   console.log(event);
+//   console.log(event.tag);
+// });
+
+// Push: Manejar las push notifications.
+self.addEventListener("push", (event) => {
+  console.log("Notificación Recibida");
+});
