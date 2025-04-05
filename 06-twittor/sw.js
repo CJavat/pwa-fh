ccf6ebe9ccdc8d88983c13932ba550f747ecab46
CJavatX <cdpm98@hotmail.com>
2025-04-05 08:04:31 -0600

    SECCIÓN 7 - TERMINADO

--- 06-twittor/sw.js
+++ 06-twittor/sw.js
@@ -1,26 +1,29 @@
-const STATIC_CACHE = "static-v1";
-const DYNAMIC_CACHE = "dynamic-v1";
+// Imports
+importScripts("js/sw-utils.js");
+
+const STATIC_CACHE = "static-v2";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/img/favicon.ico",
-  "/img/avatars/spiderman.jpg",
-  "/img/avatars/hulk.jpg",
-  "/img/avatars/ironman.jpg",
-  "/img/avatars/thor.jpg",
-  "/img/avatars/wolverine.jpg",
-  "/js/app.js",
+  // "/",
+  "index.html",
+  "css/style.css",
+  "img/favicon.ico",
+  "img/avatars/spiderman.jpg",
+  "img/avatars/hulk.jpg",
+  "img/avatars/ironman.jpg",
+  "img/avatars/thor.jpg",
+  "img/avatars/wolverine.jpg",
+  "js/app.js",
 ];
 
 const APP_SHELL_INMUTABLE = [
   "https://fonts.googleapis.com/css?family=Quicksand:300,400",
   "https://fonts.googleapis.com/css?family=Lato:400,300",
-  "/css/fontawsome.css",
-  "/css/animate.css",
-  "/js/libs/jquery.js",
+  "https://kit.fontawesome.com/2ea7eef268.js",
+  "css/animate.css",
+  "js/libs/jquery.js",
 ];
 
 self.addEventListener("install", (e) => {
@@ -41,6 +44,9 @@ self.addEventListener("activate", (e) => {
       if (key !== STATIC_CACHE && key.includes("static")) {
         return caches.delete(key);
       }
+      if (key !== DYNAMIC_CACHE && key.includes("dynamic")) {
+        return caches.delete(key);
+      }
     });
   });
 
@@ -51,7 +57,9 @@ self.addEventListener("fetch", (e) => {
   const respuesta = caches.match(e.request).then((res) => {
     if (res) return res;
 
-    return fetch(e.request).then((newRes) => {});
+    return fetch(e.request).then((newRes) => {
+      return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+    });
   });
 
   e.respondWith(respuesta);
