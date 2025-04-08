86b3d608de6dcea4355621e3114c1beccb5a7b8b
CJavatX <cdpm98@hotmail.com>
2025-04-08 05:39:58 -0600

    FIN DEL CURSOA

--- 13-paises/src/app/app.routes.ts
+++ 13-paises/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PaisesComponent } from './pages/paises/paises.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PaisesComponent },
+  { path: 'pais/:id', component: PaisesComponent },
+  { path: '**', component: PaisesComponent },
+];
